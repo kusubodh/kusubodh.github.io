eb438457fe2a8ee028ffd37e50cd9509c7d8e979
Subodh Kumar <subodh.kumar@zemosolabs.com>
2021-04-14 07:53:40 +0530

    Html and Css

--- js/action.js
+++ js/action.js
@@ -22,5 +22,18 @@ function myFunction() {
     alert(x);
 }
 
+function selectImage(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+
 // Get the element with id="defaultOpen" and click on it
-//document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click();
